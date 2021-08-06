2a82fa70fa3998839d1916f3bac796790843f457
Daniil Tsioma <d.tsioma@gmail.com>
2021-08-06 12:07:16 -0700

    Set up Contact form

--- components/contact/form/ContactForm.tsx
+++ components/contact/form/ContactForm.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { Button } from "../../general/button/Button";
 import { Formik, Form, Field, FormikValues, FormikHelpers } from "formik";
+import emailjs from "emailjs-com";
 import * as Yup from "yup";
 import styles from "./ContactForm.module.css";
 
@@ -13,6 +14,7 @@ interface ContactFormik {
 const ContactForm = () => {
   const [loading, setLoading] = useState<boolean>(false);
   const [response, setResponse] = useState<string>("");
+  const [emailError, setEmailError] = useState<boolean>(false);
 
   const ContactSchema = Yup.object().shape({
     email: Yup.string().email("invalid email :(").required("email!"),
@@ -23,19 +25,34 @@ const ContactForm = () => {
     message: Yup.string().max(1000, "too long :0").required("message!"),
   });
 
-  const handleSubmit = (
+  const handleSubmit = async (
     values: FormikValues,
     { resetForm }: FormikHelpers<ContactFormik>
   ) => {
     setLoading(true);
-    setTimeout(() => {
-      resetForm();
-      setLoading(false);
-      setResponse("Message sent!");
-      setTimeout(() => {
-        setResponse("");
-      }, 3000);
-    }, 3000);
+    emailjs
+      .sendForm(
+        process.env.EMAILJS_SERVICE_ID!,
+        process.env.EMAILJS_TEMPLATE_ID!,
+        "#contactForm",
+        process.env.EMAILJS_USER_ID!
+      )
+      .then(
+        (result) => {
+          resetForm();
+          setEmailError(false);
+          setLoading(false);
+          setResponse("Message sent!");
+          setTimeout(() => {
+            setResponse("");
+          }, 3000);
+        },
+        (error) => {
+          setEmailError(true);
+          setLoading(false);
+          setResponse("There was an error. Try again later.");
+        }
+      );
   };
 
   return (
@@ -45,7 +62,7 @@ const ContactForm = () => {
       onSubmit={handleSubmit}
     >
       {({ errors, touched }) => (
-        <Form>
+        <Form id="contactForm">
           <Field
             name="email"
             placeholder="email"
@@ -57,6 +74,8 @@ const ContactForm = () => {
             className={[styles.Control, styles.Input].join(" ")}
           />
           <Field
+            as="textarea"
+            rows="5"
             name="message"
             placeholder="message"
             className={[styles.Control, styles.Textarea].join(" ")}
--- next.config.js
+++ next.config.js
@@ -1,3 +1,8 @@
 module.exports = {
   reactStrictMode: true,
-}
+  env: {
+    EMAILJS_SERVICE_ID: process.env.EMAILJS_SERVICE_ID,
+    EMAILJS_USER_ID: process.env.EMAILJS_USER_ID,
+    EMAILJS_TEMPLATE_ID: process.env.EMAILJS_TEMPLATE_ID,
+  },
+};

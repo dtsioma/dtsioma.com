b989b83de196534ae9ee95f6edbc597e6ce86968
Daniil Tsioma <d.tsioma@gmail.com>
2021-08-06 21:43:25 -0700

    added About text

--- components/home/about/About.tsx
+++ components/home/about/About.tsx
@@ -6,14 +6,19 @@ const About: FC = () => {
     <>
       <h2 className={styles.Heading}>about me</h2>
       <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut voluptatum
-        enim corporis inventore nulla ullam! Repudiandae voluptatum sit quae
-        quasi?
+        19 y.o. Ukrainian dude living in Portland, OR. Computer Science freshman
+        in Portland State University.
       </p>
       <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut voluptatum
-        enim corporis inventore nulla ullam! Repudiandae voluptatum sit quae
-        quasi?
+        My web developer journey started in high school. At the time, I was
+        digging my way through the coal mines of freelance WordPress
+        development. Finally, I spotted the light at the{" "}
+        <i className={styles.IthoughtIvasInvisible}>front</i> end. That is where
+        I headed.
+      </p>
+      <p>
+        With three years of experience under my belt, I am excited to enter this
+        new chapter.
       </p>
     </>
   );
